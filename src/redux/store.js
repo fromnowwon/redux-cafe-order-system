91adfaa700543a329a8bffcb944eb71b986c24cf
cw <fromnowwon@gmail.com>
2024-02-03 21:03:39 +0900

    chore: redux-devtools-extension 적용

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,15 @@
-import { createStore, applyMiddleware } from 'redux';
-import rootReducer from './rootReducer';
-import logger from 'redux-logger';
+import { createStore, applyMiddleware } from "redux";
+import rootReducer from "./rootReducer";
+import logger from "redux-logger";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 // 여러 middleware 배열로 관리
 const middleware = [logger];
 
 // rootReducer를 사용하는 스토어 생성
-const store = createStore(rootReducer, applyMiddleware(...middleware));
+const store = createStore(
+	rootReducer,
+	composeWithDevTools(applyMiddleware(...middleware))
+);
 
 export default store;
